03892fd90832c483e31b276865e0a93d438195cd
Belchenko Vladimir <belchenkovova@icloud.com>
2019-07-26 10:16:39 +0300

    clean-write program

--- main.c
+++ main.c
@@ -2,64 +2,47 @@
 
 #include <gtk/gtk.h>
 
-int 				x = 0;
-char				prefix[] = "you clicked ";
-char 				postfix[] = " times";
-
-void 				make_string(char **target)
+void				func_clean(GtkWidget *widget, gpointer ptr)
 {
-	char			*number;
-	int 			number_length;
-
-	number = ft_itoa(++x);
-	number_length = ft_strlen(number);
-	(*target) = (char *)malloc(ft_strlen(prefix) + ft_strlen(postfix) + number_length + 1);
-	int i = 0;
-	for (; prefix[i]; i++)
-		(*target)[i] = prefix[i];
-	for (int j = 0; j < number_length; j++)
-		(*target)[i++] = number[j];
-	for (int j = 0; postfix[j]; j++)
-		(*target)[i++] = postfix[j];
-	(*target)[i] = '\0';
-	printf("target : %s\n", *target);
+	gtk_entry_set_text(GTK_ENTRY(ptr), "");
 }
 
-void				button_event(GtkWidget *widget, gpointer ptr)
+void				func_write(GtkWidget *widget, gpointer ptr)
 {
-	char 			*result;
-
-	make_string(&result);
-	gtk_label_set_text(GTK_LABEL(ptr), result);
-	free(result);
+	g_print("%s\n", gtk_entry_get_text(GTK_ENTRY(ptr)));
 }
 
 int					main(int argc, char **argv)
 {
 	GtkWidget		*window;
-	GtkWidget		*box_v;
-	GtkWidget		*button;
-	GtkWidget		*label;
+	GtkWidget		*table;
+	GtkWidget		*entry;
+	GtkWidget		*button_clean;
+	GtkWidget		*button_write;
+
 
 	gtk_init(&argc, &argv);
 
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	box_v = gtk_vbox_new(0,10);
-	button = gtk_button_new_with_label("button");
-	label  = gtk_label_new("you clicked 0 times");
+	table = gtk_table_new(2, 2, 1);
+	entry = gtk_entry_new();
+	button_clean = gtk_button_new_with_label("clean");
+	button_write = gtk_button_new_with_label("write");
 
 	gtk_widget_set_size_request(window, 480, 360);
-	gtk_window_set_title(GTK_WINDOW(window), "You expected a name, but it was me! Dio!");
+	gtk_window_set_title(GTK_WINDOW(window), "Window");
 
-	gtk_box_pack_start(GTK_BOX(box_v), label, 1, 1, 0);
-	gtk_box_pack_start(GTK_BOX(box_v), button, 1, 1, 0);
+	gtk_table_attach(GTK_TABLE(table), entry, 0, 2, 0, 1, GTK_FILL, GTK_FILL, 0, 0);
+	gtk_table_attach(GTK_TABLE(table), button_clean, 0, 1, 1, 2, GTK_FILL, GTK_FILL, 0, 0);
+	gtk_table_attach(GTK_TABLE(table), button_write, 1, 2, 1, 2, GTK_FILL, GTK_FILL, 0, 0);
 
-	gtk_container_add(GTK_CONTAINER(window), box_v);
+	gtk_container_add(GTK_CONTAINER(window), table);
 
 	gtk_widget_show_all(window);
 
 	g_signal_connect(window, "delete_event", G_CALLBACK(gtk_main_quit), NULL);
-	g_signal_connect(button, "clicked", G_CALLBACK(button_event), (gpointer)label);
+	g_signal_connect(button_clean, "clicked", G_CALLBACK(func_clean), (gpointer)entry);
+	g_signal_connect(button_write, "clicked", G_CALLBACK(func_write), (gpointer)entry);
 
 	gtk_main();
 
