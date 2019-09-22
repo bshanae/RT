438e20f66187ebd68b8915794c3acd0f89e623a9
Asymmetrical Shari <ashari@vo-f3.21-school.ru>
2019-09-22 13:29:59 +0300

    new scene for testing

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ void				scene_test_rt(t_scene *scene);
 void				scene_test_limited(t_scene *scene);
 void				scene_test_illumination(t_scene *scene);
 void				scene_test_rm(t_scene *scene);
+void				scene_test_box(t_scene *scene);
 void				scene_test_csg(t_scene *scene);
 
 int					main(int argc, char **argv)
@@ -16,8 +17,8 @@ int					main(int argc, char **argv)
 	gui = gui_new(&argc, &argv);
 	gui_signal_connect_all(gui);
 	gui->renderer = cl_renderer_new(gui->image);
-	scene_test_limited(gui->renderer->data.scene);
-	gui->renderer->data.camera->position.z = 90;
+	scene_test_box(gui->renderer->data.scene);
+	gui->renderer->data.camera->position.z = 180;
 	gui->renderer->data.camera->position.y = 15;
 	cl_renderer_change_tracing_mod(gui->renderer, rt_tracing_mod_rt);
 	camera_apply(gui->renderer->data.camera);
@@ -27,6 +28,40 @@ int					main(int argc, char **argv)
 	gui_loop(gui);
 	gui_delete(&gui);
 	return (0);
+}
+
+void				scene_test_box(t_scene *scene)
+{
+	object_build(scene_get_space(scene), object_type_sphere, (RT_F4_API){0., 0., 20.}, 3.);
+	scene_edit_param(scene, -1, scene_param_material, MATERIAL_LIGHT);
+	scene_edit_param(scene, -1, scene_param_name, "Light");
+
+	object_build(scene_get_space(scene), object_type_sphere, (RT_F4_API){0., 0., 10.}, 3.);
+	scene_edit_param(scene, -1, scene_param_name, "Sphere");
+
+	object_build(scene_get_space(scene), object_type_plane, (RT_F4_API){0., -50., 0.}, (RT_F4_API){0., 1., 0.});
+	scene_edit_param(scene, -1, scene_param_name, "Plane Lower");
+
+	object_build(scene_get_space(scene), object_type_plane, (RT_F4_API){0., 50., 0.}, (RT_F4_API){0., -1., 0.});
+	scene_edit_param(scene, -1, scene_param_name, "Plane Upper");
+
+	object_build(scene_get_space(scene), object_type_plane, (RT_F4_API){50., 0., 0.}, (RT_F4_API){-1., 0., 0.});
+	scene_edit_param(scene, -1, scene_param_name, "Plane Right");
+
+	object_build(scene_get_space(scene), object_type_plane, (RT_F4_API){-50., 0., 0.}, (RT_F4_API){1., 0., 0.});
+	scene_edit_param(scene, -1, scene_param_name, "Plane Left");
+
+	object_build(scene_get_space(scene), object_type_plane, (RT_F4_API){0., 0., -50.}, (RT_F4_API){0., 0., 1.});
+	scene_edit_param(scene, -1, scene_param_name, "Plane Front");
+
+	object_build(scene_get_space(scene), object_type_plane, (RT_F4_API){0., 0., 200.}, (RT_F4_API){0., 0., -1.});
+	scene_edit_param(scene, -1, scene_param_name, "Plane Back");
+
+	object_build(scene_get_space(scene), object_type_cone, (RT_F4_API){-10., -10., 0.}, (RT_F4_API){-10., 10., 0.}, 2.);
+	scene_edit_param(scene, -1, scene_param_name, "Cone");
+
+	object_build(scene_get_space(scene), object_type_cylinder, (RT_F4_API){-20., -10., 0.}, (RT_F4_API){-20., 10., 0.}, 2.);
+	scene_edit_param(scene, -1, scene_param_name, "Cylinder");
 }
 
 void				scene_test_rt(t_scene *scene)
