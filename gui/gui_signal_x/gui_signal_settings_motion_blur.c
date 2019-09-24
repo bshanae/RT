4350c7127d7eebbd513f92a88d2b1a84c75b5a5c
Vladimir Belchenko <belchenkovova@icloud.com>
2019-09-24 17:11:07 +0300

    Renderer : Motion blur doesn't flash

--- gui/gui_signal_x/gui_signal_settings_motion_blur.c
+++ gui/gui_signal_x/gui_signal_settings_motion_blur.c
@@ -7,7 +7,9 @@ gboolean			gui_signal_settings_motion_blur
 
 	gui = (t_gui *)ptr;
 	gui->renderer->data.settings.motion_blur = state;
+	gui->renderer->data.settings.motion_blur_sample_count = 0;
 	cl_renderer_flag_set(gui->renderer, cl_flag_update_settings);
 	cl_renderer_flag_set(gui->renderer, cl_flag_reset_samples);
+	gui_queue_execute_force(gui->queue);
 	return (FALSE);
 }
