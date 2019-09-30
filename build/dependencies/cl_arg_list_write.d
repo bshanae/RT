93d80285a9b3858c0c9f0da9391482822816f0fd
Vladimir Belchenko <belchenkovova@icloud.com>
2019-09-30 23:55:04 +0300

    Makefile partially

---
+++ build/dependencies/cl_arg_list_write.d
@@ -0,0 +1,20 @@
+./objects/cl_arg_list_write.o: \
+  ../cl_renderer/cl_arg_list/cl_arg_list_write.c \
+  ../cl_renderer/cl_arg_list/cl_arg_list.h ../parameters/rt_control.h \
+  ../parameters/rt_parameters.h ../error_framework/error_framework.h \
+  ../libft/includes/libft_standart.h ../cl_renderer/cl_arg_list/cl_arg.h \
+  ../parameters/rt_cl_include.h
+
+../cl_renderer/cl_arg_list/cl_arg_list.h:
+
+../parameters/rt_control.h:
+
+../parameters/rt_parameters.h:
+
+../error_framework/error_framework.h:
+
+../libft/includes/libft_standart.h:
+
+../cl_renderer/cl_arg_list/cl_arg.h:
+
+../parameters/rt_cl_include.h:
---
+++ build/dependencies/rt_background_translate.d
@@ -0,0 +1,60 @@
+./objects/rt_background_translate.o: \
+  ../cl_renderer/scene/rt_background_translate.c \
+  ../cl_renderer/scene/scene.h ../parameters/rt_parameters.h \
+  ../parameters/rt_control.h ../cl_renderer/scene/scene_point.h \
+  ../cl_renderer/object/object_interaction.h ../parameters/rt_float.h \
+  ../error_framework/error_framework.h \
+  ../libft/includes/libft_standart.h ../cl_renderer/f4/f4.h \
+  ../parameters/rt_cl_include.h \
+  ../cl_renderer/object/object_definition.h \
+  ../cl_renderer/material/material.h ../cl_renderer/object_x/object_x.h \
+  ../cl_renderer/material/material_list.h \
+  ../cl_renderer/color/color_list.h ../cl_renderer/object/object_flag.h \
+  ../cl_renderer/texture/texture.h ../parameters/rt_sources_texture.h \
+  ../cl_renderer/disruption_x/disruption.h \
+  ../cl_renderer/disruption_x/disruption_perlin_noise.h \
+  ../cl_renderer/f2/f2.h ../cl_renderer/texture/stb_image.h
+
+../cl_renderer/scene/scene.h:
+
+../parameters/rt_parameters.h:
+
+../parameters/rt_control.h:
+
+../cl_renderer/scene/scene_point.h:
+
+../cl_renderer/object/object_interaction.h:
+
+../parameters/rt_float.h:
+
+../error_framework/error_framework.h:
+
+../libft/includes/libft_standart.h:
+
+../cl_renderer/f4/f4.h:
+
+../parameters/rt_cl_include.h:
+
+../cl_renderer/object/object_definition.h:
+
+../cl_renderer/material/material.h:
+
+../cl_renderer/object_x/object_x.h:
+
+../cl_renderer/material/material_list.h:
+
+../cl_renderer/color/color_list.h:
+
+../cl_renderer/object/object_flag.h:
+
+../cl_renderer/texture/texture.h:
+
+../parameters/rt_sources_texture.h:
+
+../cl_renderer/disruption_x/disruption.h:
+
+../cl_renderer/disruption_x/disruption_perlin_noise.h:
+
+../cl_renderer/f2/f2.h:
+
+../cl_renderer/texture/stb_image.h:
